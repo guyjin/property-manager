b4a8c9675ae2bbd1633380f95c6d7ed585382aaf
David <dub@zenodub.com>
2014-08-13 18:09:25 -0600

    more stuff

--- index.js
+++ index.js
@@ -3,9 +3,22 @@ var app = express();
 
 
 
+users = [
+    {
+        username: 'david',
+        password: 'password'
+    }
+]
 
+app.set('view engine', 'ejs');
 app.use('/', express.static(__dirname + '/public'));
 
+
+app.get('/login', function(req, res){
+
+    res.render('login', { })
+
+});
 
 var server = app.listen(3000, function() {
     console.log('Listening on port %d', server.address().port);
