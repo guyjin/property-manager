84f5fc11c43dbd5cf616bb8a8b9dbc120dd5de7f
David <dub@zenodub.com>
2014-08-13 18:34:01 -0600

    more stuff

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
 
 users = [
@@ -12,6 +17,19 @@ users = [
 
 app.set('view engine', 'ejs');
 app.use('/', express.static(__dirname + '/public'));
+/*
+
+     #  #####  ####### ######  
+#     # #     # #       #     # 
+#     # #       #       #     # 
+#     #  #####  #####   ######  
+#     #       # #       #   #   
+#     # #     # #       #    #  
+ #####   #####  ####### #     # 
+
+
+
+*/
 
 
 app.get('/login', function(req, res){
@@ -19,6 +37,13 @@ app.get('/login', function(req, res){
     res.render('login', { })
 
 });
+
+
+app.post('/user/login', function(req, res){
+
+    res.send('okay!')
+})
+
 
 var server = app.listen(3000, function() {
     console.log('Listening on port %d', server.address().port);
